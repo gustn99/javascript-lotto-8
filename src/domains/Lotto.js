2db839cf7726f6936cef43c75406d5d5c0c4dd50
gustn99 <azerosong@gmail.com>
2025-10-30 13:29:14 +0900

    feat(Lotto): 숫자 범위 검증 로직 추가

--- src/domains/Lotto.js
+++ src/domains/Lotto.js
@@ -15,6 +15,14 @@ class Lotto {
     if (numbers.length > numberSet.size) {
       throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
     }
+
+    if (numbers.some((num) => num < 1)) {
+      throw new Error("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
+    }
+
+    if (numbers.some((num) => num > 45)) {
+      throw new Error("[ERROR] 로또 번호는 1에서 45 사이의 숫자여야 합니다.");
+    }
   }
 
   format() {}
