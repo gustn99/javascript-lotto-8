915aa9ea94815acbb1527609c474d51b07f0da56
gustn99 <azerosong@gmail.com>
2025-11-01 23:42:23 +0900

    feat(LottoDrawer): LottoDrawer 클래스 생성

--- src/models/LottoDrawer.js
+++ src/models/LottoDrawer.js
@@ -8,6 +8,14 @@ class LottoDrawer {
     this.#lottos = lottos;
     this.#drawnNumbers = new DrawnNumbers(winningNumbers, bonusNumber);
   }
+
+  run() {
+    const lottoArray = this.#lottos.getLottos();
+    lottoArray.forEach((lotto) => {
+      const rank = this.#drawnNumbers.calculateRank(lotto);
+      this.#lottos.win(rank);
+    });
+  }
 }
 
 export default LottoDrawer;
