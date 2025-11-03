aa57a7c1118bbcf8710c189e85f9622fcf6427ac
gustn99 <azerosong@gmail.com>
2025-11-03 19:57:10 +0900

    feat(LottoController): LottoController 구현

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,5 +1,8 @@
-import InputView from "../view/InputView";
-import OutputView from "../view/OutputView";
+import { PURCHASE_UNIT } from "../constants/unit.js";
+import LottoDrawer from "../models/LottoDrawer.js";
+import Lottos from "../models/Lottos.js";
+import InputView from "../view/InputView.js";
+import OutputView from "../view/OutputView.js";
 
 class LottoController {
   #inputView;
@@ -8,6 +11,49 @@ class LottoController {
   constructor() {
     this.#inputView = new InputView();
     this.#outputView = new OutputView();
+  }
+
+  async run() {
+    const purchaseAmount = await this.#inputView.readLineAsync(
+      "구입금액을 입력해 주세요."
+    );
+    this.#validatePurchaseAmount(purchaseAmount);
+
+    const purchaseCount = purchaseAmount / PURCHASE_UNIT;
+    const lottos = new Lottos(purchaseCount);
+    this.#outputView.print(`${purchaseAmount}개를 구매했습니다.`);
+    this.#printLottos(lottos);
+
+    const winningNumberString = await this.#inputView.readLineAsync(
+      "당첨 번호를 입력해 주세요."
+    );
+    const bonusNumberString = await this.#inputView.readLineAsync(
+      "보너스 번호를 입력해 주세요."
+    );
+
+    const lottoDrawer = new LottoDrawer(winningNumberString, bonusNumberString);
+    lottoDrawer.run(lottos);
+    this.#printResult(lottos);
+  }
+
+  #validatePurchaseAmount(purchaseAmount) {
+    if (purchaseAmount % PURCHASE_UNIT !== 0) {
+      throw new Error("[ERROR] 구입 금액은 1000원 단위로 입력해야 합니다.");
+    }
+  }
+
+  #printLottos(lottos) {
+    const formattedLottos = lottos.format();
+    this.#outputView.print(formattedLottos);
+  }
+
+  #printResult(lottos) {
+    const formattedRanks = lottos.formatResult();
+    const totalReturn = lottos.calculateTotalReturn();
+    const formattedTotalReturn = `총 수익률은 ${totalReturn}%입니다.`;
+    this.#outputView.print("당첨 통계");
+    this.#outputView.print(formattedRanks);
+    this.#outputView.print(formattedTotalReturn);
   }
 }
 
