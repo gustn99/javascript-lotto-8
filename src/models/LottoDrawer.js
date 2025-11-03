c4364783294bd2f238d36bf75083baa9519d618d
gustn99 <azerosong@gmail.com>
2025-11-03 19:52:28 +0900

    refactor(LottoDrawer): lottos를 필드에서 제거

--- src/models/LottoDrawer.js
+++ src/models/LottoDrawer.js
@@ -1,19 +1,17 @@
 import DrawnNumbers from "./DrawnNumbers.js";
 
 class LottoDrawer {
-  #lottos;
   #drawnNumbers;
 
-  constructor(lottos, winningNumbers, bonusNumber) {
-    this.#lottos = lottos;
+  constructor(winningNumbers, bonusNumber) {
     this.#drawnNumbers = new DrawnNumbers(winningNumbers, bonusNumber);
   }
 
-  run() {
-    const lottoArray = this.#lottos.getLottos();
+  run(lottos) {
+    const lottoArray = lottos.getLottos();
     lottoArray.forEach((lotto) => {
       const rank = this.#drawnNumbers.calculateRank(lotto);
-      this.#lottos.win(rank);
+      if (rank) lottos.win(rank);
     });
   }
 }
